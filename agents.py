de6924a48b9dfbb582bc451bb851ea5f8f0aa0f2
Hristo Baldzhiyski <hristo_baldjiiski04@abv.bg>
2025-07-12 12:52:58 +0200

    Attaching tools to the agents that they can use

--- agents.py
+++ agents.py
@@ -2,6 +2,36 @@ from crewai import Agent
 from textwrap import dedent
 from langchain_community.chat_models import ChatOllama  # ✅ new import
 
+from tools.search_tools import SearchTools
+from tools.calculator_tools import CalculatorTools
+
+"""
+Creating Agents Cheat Sheet:
+- Think like a boss. Work backwards from the goal and think which employee 
+    you need to hire to get the job done.
+- Define the Captain of the crew who orient the other agents towards the goal. 
+- Define which experts the captain needs to communicate with and delegate tasks to.
+    Build a top down structure of the crew.
+
+Goal:
+- Create a 7-day travel itinerary with detailed per-day plans,
+    including budget, packing suggestions, and safety tips.
+
+Captain/Manager/Boss:
+- Expert Travel Agent
+
+Employees/Experts to hire:
+- City Selection Expert 
+- Local Tour Guide
+
+
+Notes:
+- Agents should be results driven and have a clear goal in mind
+- Role is their job title
+- Goals should actionable
+- Backstory should be their resume
+"""
+
 class TravelAgents:
     def __init__(self):
         self.llm = ChatOllama(model="mistral", temperature=0.7)  # ✅ Using Ollama
@@ -17,7 +47,10 @@ class TravelAgents:
                 Create a 7-day travel itinerary with detailed per-day plans,
                 include budget, packing suggestions, and safety tips.
             """),
-           
+            tools=[
+                SearchTools.search_internet,
+                CalculatorTools.calculate
+            ],
             verbose=True,
             llm=self.llm,
         )
@@ -31,7 +64,7 @@ class TravelAgents:
             goal=dedent("""
                 Select the best cities based on weather, season, prices, and traveler interests.
             """),
-       
+             tools=[SearchTools.search_internet],
             verbose=True,
             llm=self.llm,
         )
@@ -46,7 +79,7 @@ class TravelAgents:
             goal=dedent("""
                 Provide the BEST insights about the selected city.
             """),
-            
+             tools=[SearchTools.search_internet],
             verbose=True,
             llm=self.llm,
         )
